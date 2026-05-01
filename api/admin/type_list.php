cb5547e6a9b79226a1847967a9f6e94b0ff02097
xiehuajieaa <40817160+xiehuajieaa@users.noreply.github.com>
2026-05-02 05:26:52 +0800

    Delete api directory

--- api/admin/type_list.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config.php';
-check_admin_auth();
-
-$stmt = $pdo->query("SELECT * FROM product_types ORDER BY id DESC");
-$types = $stmt->fetchAll();
-
-response($types);
-?>
